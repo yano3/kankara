c7be863b87cbb7910f92f60ffc5769c9f55fdaff
Teppei Yano <buckelwal@gmail.com>
2019-12-18 19:15:44 +0900

    set up containers

---
+++ ngx_mruby/cookie/app1/index.php
@@ -0,0 +1,8 @@
+<?php
+
+setcookie("app1_hoge", "app1_value_hoge");
+setcookie("app1_fuga", "app1_value_fuga");
+
+echo '<h1>app1</h1>';
+echo '<div><a href="/app">app</a></div>';
+
---
+++ ngx_mruby/cookie/app2/index.php
@@ -0,0 +1,11 @@
+<?php
+
+setcookie("app2_foo", "app2_value_foo");
+setcookie("app2_bar", "app2_value_bar");
+
+setcookie("app1_hoge", "app2_value_hoge");
+setcookie("app1_fuga", "app2_value_fuga");
+
+echo "<h1>app2</h1>";
+echo '<div><a href="/">TOP</a></div>';
+
