1c2e7255d32ca09cd4f76a593cb0a865efd8d1f4
Teppei Yano <buckelwal@gmail.com>
2019-12-18 19:49:12 +0900

    show cookies

--- ngx_mruby/cookie/app1/index.php
+++ ngx_mruby/cookie/app1/index.php
@@ -3,6 +3,16 @@
 setcookie("app1_hoge", "app1_value_hoge");
 setcookie("app1_fuga", "app1_value_fuga");
 
-echo '<h1>app1</h1>';
-echo '<div><a href="/app">app</a></div>';
+print '<h1>app1</h1>';
+print '<div><a href="/app">app</a></div>';
 
+print "<h2>cookies</h2>";
+
+print "<code>";
+foreach ($_COOKIE as $key => $value) {
+    print($key);
+    print ' => ';
+    print($value);
+    print '<br>';
+}
+print "</code>";
--- ngx_mruby/cookie/app2/index.php
+++ ngx_mruby/cookie/app2/index.php
@@ -6,6 +6,17 @@ setcookie("app2_bar", "app2_value_bar");
 setcookie("app1_hoge", "app2_value_hoge");
 setcookie("app1_fuga", "app2_value_fuga");
 
-echo "<h1>app2</h1>";
-echo '<div><a href="/">TOP</a></div>';
+print "<h1>app2</h1>";
+print '<div><a href="/">TOP</a></div>';
+
+print "<h2>cookies</h2>";
+
+print "<code>";
+foreach ($_COOKIE as $key => $value) {
+    print($key);
+    print ' => ';
+    print($value);
+    print '<br>';
+}
+print "</code>";
 
